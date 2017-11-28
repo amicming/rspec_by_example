fb2f4e82cb21c913dc24a17aa3079085c61b2b5f
Amit Pandya <amicming@gmail.com>
2017-11-28 15:54:51 -0800

    created Student class, now testcases are passed

--- student_details/lib/student.rb
+++ student_details/lib/student.rb
@@ -1,2 +1,18 @@
 class Student
+	def initialize(first, last)
+		@first_name = first
+		@last_name = last
+	end
+
+	def first_name
+		@first_name
+	end
+
+	def last_name
+		@last_name
+	end
+
+  def student_fullname
+  	"#{first_name} #{last_name}"
+  end
 end
