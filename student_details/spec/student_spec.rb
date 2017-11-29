17ccc8bb586d677bd13d2d3a8333eff5f12ddf77
Amit Pandya <amicming@gmail.com>
2017-11-28 16:00:48 -0800

    refactor spec file, made it more DRY

--- student_details/spec/student_spec.rb
+++ student_details/spec/student_spec.rb
@@ -1,20 +1,21 @@
 require 'student'
 
 RSpec.describe Student do
+  
+  before do
+  	@student = Student.new("Amit", "Pandya")
+  end
 
   it 'should respond to #first_name' do
-  	student = Student.new("Amit", "Pandya")
-  	expect(student).to respond_to(:first_name)
+  	expect(@student).to respond_to(:first_name)
   end
   
   it 'should respond to #last_name' do
-  	student = Student.new("Vasistha", "Pandya")
-  	expect(student).to respond_to(:last_name)
+  	expect(@student).to respond_to(:last_name)
   end
 
   it 'should respond to #student_fullname' do
-  	student = Student.new("Darshan", "Pandya")
-  	expect(student).to respond_to(:student_fullname)
+  	expect(@student).to respond_to(:student_fullname)
   end
 
 end
